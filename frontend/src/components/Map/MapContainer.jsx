bbf70985f986795e6c7baee12e2bd50c2afbf0f4
Vlad <daron-2010-777@yandex.ru>
2020-07-22 22:07:17 +0300

    WithUserSelect

--- frontend/src/components/Map/MapContainer.jsx
+++ frontend/src/components/Map/MapContainer.jsx
@@ -1,40 +1,41 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback, useContext } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { usePosition } from 'use-position';
 import {
   GoogleMap, LoadScript, Marker, Circle,
 } from '@react-google-maps/api';
 import { toSendCoordinat } from '../../redux/actions/actions';
+import UserList from '../UserList/UserLIst'
+import Context from '../../context/Context'
 
- 
 const containerStyle = {
   width: '400px',
-  height: '400px'
+  height: '400px',
 };
- 
+
 const center = {
   lat: 59.9386300,
   lng: 30.3141300,
 };
- 
-function MapContainer({data}) {
+
+function MapContainer({ data }) {
   // console.log('GEOLOCATED', geolocated.props.coords )
-  const [map, setMap] = React.useState(null)
-  const [ currentPosition, setCurrentPosition ] = useState({});
-  
-  const success = position => {
+  const [map, setMap] = React.useState(null);
+  const [currentPosition, setCurrentPosition] = useState({});
+
+  const success = (position) => {
     const currentPosition = {
       lat: position.coords.latitude,
-      lng: position.coords.longitude
-    }
+      lng: position.coords.longitude,
+    };
     setCurrentPosition(currentPosition);
   };
-  
+
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(success);
-  })
-  console.log('CURRENT_POSITION', currentPosition.lat)
-  
+  });
+  console.log('CURRENT_POSITION', currentPosition.lat);
+
   const watch = true;
   const {
     latitude,
@@ -43,34 +44,41 @@ function MapContainer({data}) {
     accuracy,
     error,
   } = usePosition(watch, { enableHighAccuracy: true });
-  
-  const dispatch = useDispatch();
-  
-  useEffect(() => {
-    dispatch(toSendCoordinat({lat: latitude, lng: longitude}));
 
-  }, [dispatch, latitude]);
-  
-  const stateEl = useSelector((state) => state.locations);
-  console.log('STATEEL', stateEl);
-  const [stateMap, setStateMap] = useState([])
-  useEffect(()=>{
-    setStateMap(stateEl)
-  }, [stateEl])
-  console.log('stateMap', stateMap)
-  const onLoad = React.useCallback(function callback(map) {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(toSendCoordinat({ lat: latitude, lng: longitude }));
+  }, [latitude]);
+
+  const state = useSelector((state) => state);
+  let stateEl = state.locations
+  let stateUsers = state.users
+  console.log('STATEEL', state);
+  const [stateMap, setStateMap] = useState([]);
+  useEffect(() => {
+    setStateMap(stateEl);
+  }, [stateEl]);
+  console.log('stateMap', stateMap);
+  const onLoad = React.useCallback((map) => {
     const bounds = new window.google.maps.LatLngBounds();
     map.fitBounds(bounds);
-    setMap(map)
-  }, [])
- 
-  const onUnmount = React.useCallback(function callback(map) {
-    setMap(null)
-  }, [])
- 
+    setMap(map);
+  }, []);
 
+  const onUnmount = React.useCallback((map) => {
+    setMap(null);
+  }, []);
 
   return (
+    <>
+    <Context.Provider value={{
+      setStateMap,
+    }}>
+      <div>
+      <button onClick={()=>{setStateMap(stateEl)}}>Full list</button>
+    <UserList list={stateUsers} />
+      </div>
     <LoadScript
       googleMapsApiKey="AIzaSyDZEnptJhEhjeXm-meAeEJJx3TkvTdO3yo"
     >
@@ -84,17 +92,16 @@ function MapContainer({data}) {
         { /* Child components, such as markers, info windows, etc. */ }
         <>
         {
-          stateMap.map((el) => {
-          return  (<Marker text={el.user} key={el.user} position={{lat: Number(el.lat), lng: Number(el.lng)}}
-              />)}
-          )
+          stateMap.map((el) => (<Marker  key={el.user} position={{ lat: Number(el.lat), lng: Number(el.lng) }}
+          />))
         }
-         
-              
+
         </>
       </GoogleMap>
     </LoadScript>
-  )
+    </Context.Provider>
+    </>
+  );
 }
- 
-export default React.memo(MapContainer)
+
+export default React.memo(MapContainer);
